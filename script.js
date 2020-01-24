856a47ce61f8d7317ac3d976f76fae51fad40bc3
manimani1126 <3906mani@gmail.com>
2020-01-24 15:42:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleNav() {
+    var body = document.body;
+    var hamburger = document.getElementById('js-hamburger');
+    var blackBg = document.getElementById('js-black-bg');
+  
+    hamburger.addEventListener('click', function() {
+      body.classList.toggle('nav-open');
+    });
+    blackBg.addEventListener('click', function() {
+      closeNav()
+    });
+  }
+  toggleNav();
+  function closeNav(){
+    document.body.classList.remove('nav-open');
+  }
